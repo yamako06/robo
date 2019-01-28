2d4c47e4d7169bbdee208d6490138b6a1eae11c6
yamako06 <45563933+yamako06@users.noreply.github.com>
2019-01-28 22:30:11 +0900

    Add files via upload

--- SRC_kanai_teaching.py
+++ SRC_kanai_teaching.py
@@ -12,6 +12,7 @@ import pandas as pd
 import time
 import csv
 
+
 ###変数
 x=[]
 J1=[]
@@ -29,6 +30,10 @@ Delay_1000=[2,31,87,89,136,142,148,156,164,166,189,194]
 Delay_2000=[78,95,119,120,154,167]
 Pos_Value=[0.0 , 0.0 , 90.0 , 0.0 , 90.0 , 0.0]
 
+###RC8 or VRC
+#control_mode = "RC8"
+control_mode = "VRC"
+
 
 ###座標格納関数
 def get_move_pos(J_pos,J1,J2,J3,J4,J5,J6,P):
@@ -41,28 +46,30 @@ def get_move_pos(J_pos,J1,J2,J3,J4,J5,J6,P):
     return J_pos
 
 
-###ロボット制御用関数
+
+###ロボット制御用関数(Comp1:Move P,2:Move L,3:Move C,4:Move S)
 def robot_cont(Command,Speed,Accel,Decel,Comp,Pos_value,Mode1,Mode2):
     ### set ExtSpeed,Accel,Decel
     if Command==1:
         Command = "ExtSpeed"
-    #Speed = 20
-    #Accel = 25
-    #Decel = 25
     Param = [Speed,Accel,Decel]
     m_bcapclient.robot_execute(HRobot,Command,Param)
     print("ExtSpeed")
-    ### Move Position
-    #Comp=1
+    
+    ###Select Mode
     if Mode1==1:
         Mode1 = "J"
+    elif Mode1 == 2:
+        Mode1 = "P"
     if Mode2==1:
         Mode2 = "@P"
-    elif Mode == 2:
+    elif Mode2 == 2:
         Mode2 = "@E"
+
+    ### Move Position
     Pose = [Pos_value,Mode1,Mode2]
     m_bcapclient.robot_move(HRobot,Comp,Pose,"")
-    print("Complete Move:",Pos_value)
+    print("Complete Move:",Pos_value,i)
 
 ###入力データ
 csvfile = 'SRC_J.csv'
@@ -89,9 +96,14 @@ J4=list(map(float, J4))
 J5=list(map(float, J5))
 J6=list(map(float, J6))
 
+
 #--------------------------------前処理--------------------------------#
-### set IP Address , Port number and Timeout of connected RC8
-host = "192.168.0.1"
+
+### set IP Address , Port number and Timeout of connected RC8 or VRC
+if control_mode == "RC8":
+    host = "192.168.0.1"
+elif control_mode == "VRC":
+    host = "192.168.0.20"
 port = 5007
 timeout = 2000
 
@@ -128,14 +140,22 @@ Param = [1,0]
 m_bcapclient.robot_execute(HRobot,Command,Param)
 print("Motor On")
 
-#--------------------------------ロボット動作--------------------------------#
-for i in range(1,201):
-    ###座標の設定は引数8番目の変える（P8なら8にする、SRC_組立動作(J型)フローのNo8に対応）
+Comp=1
+Pos_value = [0.0 , 0.0 , 90.0 , 0.0 , 90.0 , 0.0]
+Pose = [Pos_value,"J","@E"]
+m_bcapclient.robot_move(HRobot,Comp,Pose,"")
+print("Complete Move P,@E J(0.0, 0.0, 90.0, 0.0, 90.0, 0.0)")
+time.sleep(1)
+
+#--------------------------------ロボット動作命令部--------------------------------#
+for i in range(1,202):
+    ###座標の設定は引数8番目の変える（P8なら8にする、SRC_組立動作(J型)フローのNo8に対応
     Pos_value = get_move_pos(Pos_value,J1,J2,J3,J4,J5,J6,i)
-    if i == 201:
-        robot_cont(1,50,25,25,1,Pos_value,1,2)
+
+    if i == 202:
+        robot_cont(1,10,25,25,1,Pos_value,1,2)
     else:
-        robot_cont(1,50,25,25,1,Pos_value,1,1)
+        robot_cont(1,10,25,25,2,Pos_value,1,1)
     #time.sleep(0.5)
     if i in Delay_300:
         delay=0.3
@@ -152,6 +172,7 @@ for i in range(1,201):
     else:
         delay=0
     time.sleep(delay)
+
 
 #--------------------------------後処理--------------------------------#
 ###Motor Off
@@ -177,10 +198,9 @@ if hCtrl != 0:
 ### b-cap service stop
 m_bcapclient.service_stop()
 print("b-cap service Stop")
-
 del m_bcapclient
-
 print("Finish")
+
 
 
 """サンプル
@@ -193,12 +213,10 @@ Param = [Speed,Accel,Decel]
 m_bcapclient.robot_execute(HRobot,Command,Param)
 print("ExtSpeed")
 
-
 ### Move Initialize Position
 Comp=1
 Pos_value = [0.0 , 0.0 , 90.0 , 0.0 , 90.0 , 0.0]
 Pose = [Pos_value,"J","@E"]
 m_bcapclient.robot_move(HRobot,Comp,Pose,"")
 print("Complete Move P,@E J(0.0, 0.0, 90.0, 0.0, 90.0, 0.0)")
-
 """
